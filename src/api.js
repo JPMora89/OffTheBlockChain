a26c4dd53ee192966418867f05c81d2496d21568
Pablo M <jpmora123@yahoo.com>
2023-08-29 09:38:51 -0400

    attempting to deply

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
  * Static class tying together methods used to get/send to to the API.
  * There shouldn't be any frontend-specific stuff here, and there shouldn't
  * be any API-aware stuff elsewhere in the frontend.
- *
+ 
  */
 
 class OtbcApi {
