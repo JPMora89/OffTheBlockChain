ee8c1fd69fd6c4b79f7b742ecc1cf4ed10a5cdf9
Pablo M <jpmora123@yahoo.com>
2023-09-04 16:28:36 -0400

    changing the news API in use

--- src/coinComponents/coinNews.js
+++ src/coinComponents/coinNews.js
@@ -1,3 +1,52 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import "./coinDetail.css"
+
+// const CoinNews = ({ coinId }) => {
+//   const [news, setNews] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchCoinNews = async () => {
+//       try {
+//         const response = await axios.get(
+//           `https://newsapi.org/v2/everything?q=${coinId}&apiKey=aa38df6ade6f4268bb8484a411245302`
+//         );
+//         setNews(response.data.articles);
+//         setIsLoading(false);
+//       } catch (error) {
+//         console.error("Error fetching coin news:", error);
+//         setIsLoading(false);
+//       }
+//     };
+
+//     if (coinId) {
+//       fetchCoinNews();
+//     }
+//   }, [coinId]);
+
+//   return (
+//     <div className="coinNews">
+//       <h2 id="coinnewsheader">Coin News</h2>
+//       {isLoading ? (
+//         <p>Loading news...</p>
+//       ) : (
+//         <ul>
+//           {news.slice(0, 5).map((article) => (
+//             <li key={article.id}>
+//               <a href={article.url} target="_blank" rel="noopener noreferrer">
+//                 {article.title}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default CoinNews;
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./coinDetail.css"
@@ -8,11 +57,15 @@ const CoinNews = ({ coinId }) => {
 
   useEffect(() => {
     const fetchCoinNews = async () => {
+      if (!coinId) {
+        return; // Don't make a request if coinId is not defined
+      }
+
       try {
         const response = await axios.get(
-          `https://newsapi.org/v2/everything?q=${coinId}&apiKey=aa38df6ade6f4268bb8484a411245302`
+          `https://cryptopanic.com/api/v1/posts/?auth_token=12a2442613bd9a59f7b47b9c8da4001642d3b126&public=true&currencies=${coinId}`
         );
-        setNews(response.data.articles);
+        setNews(response.data.results);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching coin news:", error);
@@ -20,9 +73,7 @@ const CoinNews = ({ coinId }) => {
       }
     };
 
-    if (coinId) {
-      fetchCoinNews();
-    }
+    fetchCoinNews();
   }, [coinId]);
 
   return (
@@ -46,3 +97,4 @@ const CoinNews = ({ coinId }) => {
 };
 
 export default CoinNews;
+
