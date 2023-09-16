d5e62bb057249b97cd73bdf6bbe9c1fa3e56e3d1
Pablo M <jpmora123@yahoo.com>
2023-09-15 22:23:36 -0400

    added tests

--- src/knowledgeComponents/KnowledgeContainer.test.js
+++ src/knowledgeComponents/KnowledgeContainer.test.js
@@ -6,10 +6,8 @@ describe('KnowledgeContainer', () => {
   it('renders the knowledge topics and images', () => {
     render(<KnowledgeContainer />);
 
-    // Assert that the main title is rendered
     expect(screen.getByText('Knowledge')).toBeInTheDocument();
 
-    // Assert that the knowledge topics are rendered
     expect(screen.getByText('Introduction to Cryptocurrencies')).toBeInTheDocument();
     expect(screen.getByText('Blockchain Technology: The building blocks of trust and transparency')).toBeInTheDocument();
     expect(screen.getByText('Decentralized Finance (DeFi)')).toBeInTheDocument();
@@ -17,7 +15,6 @@ describe('KnowledgeContainer', () => {
     expect(screen.getByText('Smart Contracts and Decentralized Applications (DApps)')).toBeInTheDocument();
     expect(screen.getByText('How to Get Started Trading Crypto')).toBeInTheDocument();
 
-    // Assert that the images are rendered
     expect(screen.getByAltText('blockchain')).toBeInTheDocument();
     expect(screen.getByAltText('defi')).toBeInTheDocument();
   });
--- src/knowledgeComponents/knowledgehardcoded.test.js
+++ src/knowledgeComponents/knowledgehardcoded.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; // Use MemoryRouter for routing-related tests
+import { MemoryRouter } from 'react-router-dom'; 
 import KnowledgeHardCode from './KnowledgeHardCode';
 
 describe('KnowledgeHardCode', () => {
@@ -11,10 +11,8 @@ describe('KnowledgeHardCode', () => {
       </MemoryRouter>
     );
 
-    // Assert that the main title is rendered
     expect(screen.getByText('What you need to know about BlockChain technology')).toBeInTheDocument();
 
-    // Assert that the knowledge topics are rendered
     expect(screen.getByText('Introduction to Cryptocurrencies')).toBeInTheDocument();
     expect(screen.getByText('Blockchain Technology: The building blocks of trust and transparency')).toBeInTheDocument();
     expect(screen.getByText('Decentralized Finance (DeFi)')).toBeInTheDocument();
@@ -22,10 +20,8 @@ describe('KnowledgeHardCode', () => {
     expect(screen.getByText('Smart Contracts and Decentralized Applications (DApps)')).toBeInTheDocument();
     expect(screen.getByText('How to Get Started Trading Crypto')).toBeInTheDocument();
 
-    // Assert that the Glossary link is rendered
     expect(screen.getByText('Glossary')).toBeInTheDocument();
 
-    // Assert that the images are rendered (you can also use 'getByAltText')
     expect(screen.getByTestId('blockchainTransformed')).toBeInTheDocument();
     expect(screen.getByTestId('defitransformed')).toBeInTheDocument();
     expect(screen.getByTestId('dApps')).toBeInTheDocument();
